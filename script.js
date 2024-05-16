cd93753639b60bfb58e045f0b3462394ac45733a
Shiva Kumar Karru <103453725+Shivaksk119@users.noreply.github.com>
2024-05-17 00:26:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	this.name = name;
+	this.age = age;
+}
 
-function Employee(name, age, jobTitle) {}
+Person.prototype.greet = ()=>{
+	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
+}
+
+function Employee(name, age, jobTitle) {
+	Person.call(this,name, age);
+	this.jobTitle = jobTitle;
+}
+
+Object.setPrototypeOf(Employee,Person);
+
+Employee.prototype.jobGreet = ()=>{
+	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`)
+}
 
 // Do not change code below this line
 window.Person = Person;
