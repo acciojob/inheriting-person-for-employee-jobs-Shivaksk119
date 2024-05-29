a7b0a9a5bed51da313a99a9d1d37cd91daa80170
Shiva Kumar Karru <103453725+Shivaksk119@users.noreply.github.com>
2024-05-30 00:33:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,41 +1,25 @@
 // complete this js code
-let perObj = {
-	greet(){
-	 	console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`)
+class Person{
+	constructor(name,age){
+		this.name = name;
+		this.age = age;
+	}
+
+	greet() {
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
 	}
 }
 
-function Person(name, age) {
-	const user = Object.create(perObj);
-	user.name = name;
-	user.age = age;
-	return user
-}
+class Employee extends Person{
+	constructor(name,age,jobTitle){
+		super(name,age);
+		this.jobTitle = jobTitle;
+	}
 
-// Person.prototype.greet = ()=>{
-// 	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
-// }
-
-let empObj = {
 	jobGreet() {
-		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`);
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`)
 	}
 }
-
-Object.setPrototypeOf(empObj,perObj);
-
-function Employee(name, age, jobTitle) {
-	const user1 = Object.create(empObj);
-	user1.name = name;
-	user1.age = age;
-	user1.jobTitle = jobTitle;
-	return user1
-}
-
-// Employee.prototype.jobGreet = ()=>{
-// 	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`)
-// }
-
 
 // Do not change code below this line
 window.Person = Person;
